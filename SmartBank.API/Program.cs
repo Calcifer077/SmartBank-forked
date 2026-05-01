ba2df62bf51690d2f2fa234941d872a0530af3af
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-05-01 18:28:40 +0530

    enabled swagger in prod

--- SmartBank.API/Program.cs
+++ SmartBank.API/Program.cs
@@ -130,7 +130,7 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
